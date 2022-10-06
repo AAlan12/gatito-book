38189482a464e5d3efe66870bd4155f7e2a79552
Antonio Alan <tonio.alan07@gmail.com>
2022-10-06 10:28:54 -0300

    interação Http, injeção de dependência e validação do forms de login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,11 @@
+import { MessageModule } from './../components/message/message.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule,
+    MessageModule
   ],
   exports:[
     HomeComponent
